8bb2127afc3bdc97111c08d5d1538212e0ad440e
Majid Kargar <52089654+fulcain@users.noreply.github.com>
2023-03-11 16:02:52 +0330

    added netflix icon / fixed accordion (FAQ)
    - added netflix icon
    - fixed accordion box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,36 @@
 // variables 
 const
-    headerElements = document.querySelectorAll('.accordion-header');
+    headerElements = document.querySelectorAll('.accordion-header'),
+    bodyElements = document.querySelectorAll('.accordion-body');
 
 // events
 
 headerElements.forEach(header => {
+
     // all headers
     header.addEventListener('click', () => {
-        if (header.parentElement.children[1].getAttribute("data-is-open") == "false") {
-            header.parentElement.children[1].setAttribute("data-is-open", "true")
-            header.parentElement.children[1].classList.add("active")
-        } else {
+
+        // if the header has active class
+        // class close All items function
+        if (header.parentElement.children[1].classList.contains("active")) {
+            closeAllItems()
             header.parentElement.children[1].classList.remove("active")
-            header.parentElement.children[1].setAttribute("data-is-open", "false")
+
+        }
+        
+        // if the header doesn't have active class
+        // class close All items function
+        else {
+            closeAllItems()
+            header.parentElement.children[1].classList.add("active")
         }
     })
 })
+
+// TITLE: close all items
+// closes all body elements
+function closeAllItems() {
+    bodyElements.forEach(body => {
+        body.classList.remove("active")
+    })
+}
